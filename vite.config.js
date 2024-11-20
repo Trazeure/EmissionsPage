59fb67d05d84e40217251c04319497a2ebc67b4b
Luis Angel <destiny72002@outlook.com>
2024-11-20 15:19:35 -0600

    Actualización de vite.config.js para GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -2,14 +2,15 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/EmissionsPage/',  // Configuración añadida para desplegar en GitHub Pages
   plugins: [react()],
   server: {
-    host: true,     // Añadido para permitir acceso desde la red
+    host: true,     // Permite acceso desde la red local
     port: 3000      // Tu puerto actual
   },
   resolve: {
     alias: {
-      '@': '/src',  // Manteniendo tu alias actual
+      '@': '/src',  // Alias para tu estructura de archivos
     },
   }
 })
